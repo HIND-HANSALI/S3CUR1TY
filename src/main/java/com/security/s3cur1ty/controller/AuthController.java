1ec071189e436dfef8ca12c260e06a9914ceb689
HIND-HANSALI <hindhsl111@gmail.com>
2024-02-01 14:07:45 +0100

    add controller methods

--- src/main/java/com/security/s3cur1ty/controller/AuthController.java
+++ src/main/java/com/security/s3cur1ty/controller/AuthController.java
@@ -1,2 +1,31 @@
-package com.security.s3cur1ty.controller;public class AuthController {
+package com.security.s3cur1ty.controller;
+
+import com.security.s3cur1ty.dto.request.AuthenticationRequest;
+import com.security.s3cur1ty.dto.request.RegisterRequest;
+import com.security.s3cur1ty.dto.response.AuthenticationResponse;
+import com.security.s3cur1ty.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("test");
+    }
 }
